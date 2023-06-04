35262b4a194f67e7d20aa10ad30fc3a485df7126
hirotsugu <hiro03@tsuguMac.local>
2023-06-04 17:26:52 +0900

    HomeとLoginの簡易画面作成とrouterの設定

--- gacha/vuejs/vuejs/src/App.vue
+++ gacha/vuejs/vuejs/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/login">Login</router-link></li>
+     </ul>
+     <router-view/>
   </div>
 </template>
 
@@ -9,20 +12,6 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
