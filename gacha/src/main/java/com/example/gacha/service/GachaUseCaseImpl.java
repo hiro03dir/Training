0dea2469487308fb661da3c041d244e9265f5974
hirotsugu <hiro03@tsuguMac.local>
2023-07-29 18:41:47 +0900

    トランザクションの実装

--- gacha/src/main/java/com/example/gacha/service/GachaUseCaseImpl.java
+++ gacha/src/main/java/com/example/gacha/service/GachaUseCaseImpl.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@Transactional
 public class GachaUseCaseImpl implements GachaUseCase {
     @Autowired
     private UserRepository userRepository;
